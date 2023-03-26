99d55220f5ac3a6a0912723ee08c2d00ae2cffed
Jaebock.cho <last2599@gmail.com>
2023-03-26 20:30:20 +0900

    셋업

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import _ from 'lodash';
+const path = require('path');
 
-function component() {
-  const element = document.createElement('div');
-
-  // 이제 Lodash를 스크립트로 가져왔습니다.
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
