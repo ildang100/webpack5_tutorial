fb7fd3b76a04ca37c3439c1bcc8f1dd492d2fc10
Jaebock.cho <last2599@gmail.com>
2023-03-26 21:22:21 +0900

    Dynamic Imports

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,9 @@ module.exports = {
   mode: 'development',
   entry: {
     index: './src/index.js',
-    another: './src/another-module.js',
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
-  },
 };
-
-// mini-css-extract-plugin : 메인 애플리케이션에서 CSS를 분리하는데 유용합니다.
