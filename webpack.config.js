ff41be20ea981ce57ed187fe4ab06f76eb1faf2e
Jaebock.cho <last2599@gmail.com>
2023-03-29 23:22:39 +0900

    Authoring a Library

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,21 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Caching',
-    }),
-  ],
   output: {
-    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
+    filename: 'webpack-numbers.js',
+    library: {
+      name: "webpackNumbers",
+      type: "umd"
+    },
   },
-  optimization: {
-    moduleIds: 'deterministic',
-    runtimeChunk: 'single',
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
-        },
-      },
+  externals: {
+    lodash: {
+      commonjs: 'lodash',
+      commonjs2: 'lodash',
+      amd: 'lodash',
+      root: '_',
     },
   },
 };
